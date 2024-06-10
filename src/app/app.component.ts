58f4653d5bebcec059ca5efbf7a6856e907c34cd
Kamil Chmielowski <kamil.chmielowski@comarch.com>
2024-06-10 08:02:10 +0200

    feat: footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { ChangeDetectionStrategy, Component, HostBinding, OnInit } from '@angular/core';
+import {
+  NavigationEnd,
+  Router,
+  RouterLink,
+  RouterOutlet,
+  RoutesRecognized
+} from '@angular/router';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
+import { filter, map } from 'rxjs';
 import { SvgIconComponent, SvgIconRegistryService } from 'angular-svg-icon';
 
 import { appIconsMap } from './app-icons-map';
 import { AsideComponent } from './components/aside/aside.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
@@ -14,13 +23,32 @@ import { AsideComponent } from './components/aside/aside.component';
     RouterLink,
     RouterOutlet,
     SvgIconComponent,
+    FooterComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent implements OnInit {
-  constructor(private registry: SvgIconRegistryService) {}
+  @HostBinding('class.homepage') protected isHome = false;
+
+  private observeRouterEvents(): void {
+    this.router.events
+      .pipe(
+        takeUntilDestroyed(),
+        filter(val => val instanceof NavigationEnd),
+        map(val => val as RoutesRecognized),
+      ).subscribe((val) => {
+        this.isHome = val.urlAfterRedirects === '/';
+    });
+  }
+
+  constructor(
+    private registry: SvgIconRegistryService,
+    private router: Router,
+  ) {
+    this.observeRouterEvents();
+  }
 
   ngOnInit() {
     this.registerIcons();
---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,19 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SvgIconComponent } from 'angular-svg-icon';
+
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    CommonModule,
+    SvgIconComponent,
+  ],
+})
+export class FooterComponent {
+
+}
