7add723d775e80cc041ca2d611b907e319e5aea3
Kamil Chmielowski <kamil.chmielowski@comarch.com>
2024-06-12 07:17:38 +0200

    feat: copy code to clipboard

--- src/app/components/code/code.component.ts
+++ src/app/components/code/code.component.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ClipboardModule } from '@angular/cdk/clipboard';
 
 import { Highlight, HighlightAuto } from 'ngx-highlightjs';
 import { HighlightLineNumbers } from 'ngx-highlightjs/line-numbers';
 
 import 'highlight.js/styles/an-old-hope.css';
+import { SvgIconComponent } from 'angular-svg-icon';
 // import 'highlight.js/styles/atom-one-dark.min.css';
 // import 'highlight.js/styles/atom-one-dark-reasonable.min.css';
 // import 'highlight.js/styles/github-dark.min.css';
@@ -12,9 +14,11 @@ import 'highlight.js/styles/an-old-hope.css';
   selector: 'app-code',
   standalone: true,
   imports: [
+    ClipboardModule,
     HighlightAuto,
     HighlightLineNumbers,
     Highlight,
+    SvgIconComponent,
   ],
   templateUrl: './code.component.html',
   styleUrl: './code.component.scss',
